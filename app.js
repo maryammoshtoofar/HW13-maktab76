0289e5d04bfba36f7e07470f9a4a8b36fe035acc
Maryam Moshootfar <maryammoshtoofar@gmail.com>
2022-06-29 12:53:30 +0430

    added all functionalitites

---
+++ app.js
@@ -0,0 +1,77 @@
+const API_URL = "https://62ab6beda62365888bdc2f11.mockapi.io/Hw13";
+const siteIds = [];
+const projectIds = [];
+const tbl = document.getElementById("targets");
+const siteIdRow = document.getElementById("tableHead");
+
+document.addEventListener("DOMContentLoaded", () => {
+  getData();
+});
+
+// Fetch
+const getData = () => {
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item) => {
+        siteIds.push(item.SiteId);
+        projectIds.push(item.ProjectId);
+      });
+      generateSiteIdCells(sortSiteIds(siteIds));
+      generateTargetArrays(
+        sortSiteIds(siteIds),
+        sortProjectIds(projectIds),
+        data
+      );
+    });
+};
+
+const sortSiteIds = (siteIds) => {
+  const sortedSiteIds = [...new Set(siteIds)].sort((a, b) => {
+    return a - b;
+  });
+
+  return sortedSiteIds;
+};
+
+const generateSiteIdCells = (siteIds) => {
+  for (let i = 0; i < siteIds.length; i++) {
+    const siteIdCell = document.createElement("th");
+    siteIdCell.innerHTML = siteIds[i];
+    siteIdRow.appendChild(siteIdCell);
+  }
+  tbl.appendChild(siteIdRow);
+};
+
+const sortProjectIds = (projectIds) => {
+  const sortedProjectIds = [...new Set(projectIds)].sort((a, b) => {
+    return a - b;
+  });
+  return sortedProjectIds;
+};
+
+const generateTargetArrays = (siteIds, projectIds, data) => {
+  for (let i = 0; i < projectIds.length; i++) {
+    const targetArray = [];
+    targetArray.push(projectIds[i]);
+    for (let j = 0; j < siteIds.length; j++) {
+      for (let item of data) {
+        if (item.ProjectId === projectIds[i] && item.SiteId === siteIds[j]) {
+          targetArray.push(item.Target);
+        }
+      }
+    }
+
+    generateTargetCells(targetArray);
+  }
+};
+
+const generateTargetCells = (targetArray) => {
+  const targetRow = document.createElement("tr");
+  for (let i = 0; i < targetArray.length; i++) {
+    const targetCells = document.createElement("td");
+    targetCells.innerHTML = targetArray[i];
+    targetRow.appendChild(targetCells);
+  }
+  tbl.appendChild(targetRow);
+};
